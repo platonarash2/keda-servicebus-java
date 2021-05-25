d7b9c3b0a4f69613cf3df36d6244e9736fe7e709
platonarash2 <73690233+platonarash2@users.noreply.github.com>
2021-05-25 12:17:49 +0200

    created

---
+++ src/com/company/Main.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import com.azure.messaging.servicebus.*;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    static String connectionString = "<connectionString to the service queue>";
+    static String queueName = "<name of the service bus queue>";
+
+    public static void main(String[] args) {
+        try {
+            // write your code here
+            System.out.println("Starting the consumer...");
+            receiveMessages();
+        }
+        catch (java.lang.InterruptedException ex)
+        {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    static void receiveMessages() throws InterruptedException
+    {
+        CountDownLatch countdownLatch = new CountDownLatch(1);
+
+        // Create an instance of the processor through the ServiceBusClientBuilder
+        ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .processor()
+                .queueName(queueName)
+                .processMessage(Main::processMessage)
+                .processError(context -> processError(context, countdownLatch))
+                .buildProcessorClient();
+
+        System.out.println("Starting the processor");
+        processorClient.start();
+
+        while (true);
+
+        //TimeUnit.SECONDS.sleep(10);
+        //System.out.println("Stopping and closing the processor");
+        //processorClient.close();
+    }
+
+    private static void processMessage(ServiceBusReceivedMessageContext context) {
+        ServiceBusReceivedMessage message = context.getMessage();
+        System.out.printf("Processing message. Session: %s, Sequence #: %s. Contents: %s%n", message.getMessageId(),
+                message.getSequenceNumber(), message.getBody());
+    }
+
+
+    private static void processError(ServiceBusErrorContext context, CountDownLatch countdownLatch) {
+        System.out.printf("Error when receiving messages from namespace: '%s'. Entity: '%s'%n",
+                context.getFullyQualifiedNamespace(), context.getEntityPath());
+
+        if (!(context.getException() instanceof ServiceBusException)) {
+            System.out.printf("Non-ServiceBusException occurred: %s%n", context.getException());
+            return;
+        }
+
+        ServiceBusException exception = (ServiceBusException) context.getException();
+        ServiceBusFailureReason reason = exception.getReason();
+
+        if (reason == ServiceBusFailureReason.MESSAGING_ENTITY_DISABLED
+                || reason == ServiceBusFailureReason.MESSAGING_ENTITY_NOT_FOUND
+                || reason == ServiceBusFailureReason.UNAUTHORIZED) {
+            System.out.printf("An unrecoverable error occurred. Stopping processing with reason %s: %s%n",
+                    reason, exception.getMessage());
+
+            countdownLatch.countDown();
+        } else if (reason == ServiceBusFailureReason.MESSAGE_LOCK_LOST) {
+            System.out.printf("Message lock lost for message: %s%n", context.getException());
+        } else if (reason == ServiceBusFailureReason.SERVICE_BUSY) {
+            try {
+                // Choosing an arbitrary amount of time to wait until trying again.
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.err.println("Unable to sleep for period of time");
+            }
+        } else {
+            System.out.printf("Error source %s, reason %s, message: %s%n", context.getErrorSource(),
+                    reason, context.getException());
+        }
+    }
+}
